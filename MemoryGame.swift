9ae62e701f06f21739c64c38377f866f7e967a93
Ainash <turbayeva17@gmail.com>
2023-04-20 15:53:21 +0600

    Refactor code, add card deal animation, implement card disappearance on match, animate matched cards, shuffle functionality, and game restart feature"

--- MemoryGame.swift
+++ MemoryGame.swift
@@ -11,46 +11,134 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
     
     private(set) var cards: Array<Card>
     
-    private var indexOfTheOneAndOnlyOneFaceUpCard: Int?
-    
-     mutating func choose(_ card: Card){
-         if let chosenIndex = cards.firstIndex(where: {$0.id == card.id }),
-            !cards[chosenIndex].isFaceUp,
-            !cards[chosenIndex].isMatched
-         {
-             if let potentialMatchIndex = indexOfTheOneAndOnlyOneFaceUpCard {
-                 if cards[chosenIndex].content == cards[potentialMatchIndex].content {
-                     cards[chosenIndex].isMatched = true
-                     cards[potentialMatchIndex].isMatched = true
-                 }
-                 indexOfTheOneAndOnlyOneFaceUpCard = nil
-             }else {
-                 for index in cards.indices {
-                     cards[index].isFaceUp = false
-                 }
-                 indexOfTheOneAndOnlyOneFaceUpCard = chosenIndex
-             }
-             cards[chosenIndex].isFaceUp.toggle()
-         }
-        print("\(cards)")
+    private var indexOfTheOneAndOnlyOneFaceUpCard: Int? {
+        get { cards.indices.filter({index in cards[index].isFaceUp}).oneAndOnly }
+        set { // isFaceUp is only true if the index is the newValue
+            cards.indices.forEach { cards[$0].isFaceUp = ($0 == newValue)} // {index in cards[index].isFaceUp = (index == newValue)}
+            //            for index in cards.indices {
+            //                cards[index].isFaceUp = (index == newValue)
+            //            }
+        }
     }
     
-
+    mutating func choose(_ card: Card){
+        if let chosenIndex = cards.firstIndex(where: {$0.id == card.id }),
+           !cards[chosenIndex].isFaceUp,
+           !cards[chosenIndex].isMatched
+        {
+            if let potentialMatchIndex = indexOfTheOneAndOnlyOneFaceUpCard {
+                if cards[chosenIndex].content == cards[potentialMatchIndex].content {
+                    cards[chosenIndex].isMatched = true
+                    cards[potentialMatchIndex].isMatched = true
+                }
+                cards[chosenIndex].isFaceUp = true
+            }else {
+                indexOfTheOneAndOnlyOneFaceUpCard = chosenIndex
+            }
+            
+        }
+    }
+    
+    mutating func shuffle() {
+        cards.shuffle()
+    }
+    
     
     init(numberOfPairOfCards: Int, createContent: (Int) -> CardContent){
-        cards = Array<Card>()
+        cards = []
         for pairIndex in 0..<numberOfPairOfCards{
             let content:CardContent = createContent(pairIndex)
             cards.append(Card(content: content, id: pairIndex*2))
             cards.append(Card(content: content, id: pairIndex*2+1))
         }
-        
+        cards.shuffle()
     }
     
     struct Card: Identifiable{
-        var isFaceUp: Bool = false
-        var isMatched: Bool = false
-        var content: CardContent
-        var id: Int
+        var isFaceUp = false {
+            didSet {
+                if isFaceUp {
+                    startUsingBonusTime()
+                } else {
+                    stopUsingBonusTime()
+                }
+            }
+        }
+        var isMatched = false {
+            didSet {
+                stopUsingBonusTime()
+            }
+        }
+        let content: CardContent
+        let id: Int
+        
+        // MARK: - Bonus Time
+        
+        // this could give matching bonus points
+        // if the user matches the card
+        // before a certain amount of time passes during which the card is face up
+        
+        // can be zero which means "no bonus available" for this card
+        var bonusTimeLimit: TimeInterval = 6
+        
+        // how long this card has ever been face up
+        private var faceUpTime: TimeInterval {
+            if let lastFaceUpDate = self.lastFaceUpDate {
+                return pastFaceUpTime + Date().timeIntervalSince(lastFaceUpDate)
+            } else {
+                return pastFaceUpTime
+            }
+        }
+        // the last time this card was turned face up (and is still face up)
+        var lastFaceUpDate: Date?
+        // the accumulated time this card has been face up in the past
+        // (i.e. not including the current time it's been face up if it is currently so)
+        var pastFaceUpTime: TimeInterval = 0
+        
+        // how much time left before the bonus opportunity runs out
+        var bonusTimeRemaining: TimeInterval {
+            max(0, bonusTimeLimit - faceUpTime)
+        }
+        // percentage of the bonus time remaining
+        var bonusRemaining: Double {
+            (bonusTimeLimit > 0 && bonusTimeRemaining > 0) ? bonusTimeRemaining/bonusTimeLimit : 0
+        }
+        // whether the card was matched during the bonus time period
+        var hasEarnedBonus: Bool {
+            isMatched && bonusTimeRemaining > 0
+        }
+        // whether we are currently face up, unmatched and have not yet used up the bonus window
+        var isConsumingBonusTime: Bool {
+            isFaceUp && !isMatched && bonusTimeRemaining > 0
+        }
+        
+        // called when the card transitions to face up state
+        private mutating func startUsingBonusTime() {
+            if isConsumingBonusTime, lastFaceUpDate == nil {
+                lastFaceUpDate = Date()
+            }
+        }
+        // called when the card goes back face down (or gets matched)
+        private mutating func stopUsingBonusTime() {
+            pastFaceUpTime = faceUpTime
+            self.lastFaceUpDate = nil
+        }
+        
+    }
+    
+    
+}
+// add an extension to an array
+// oneAndOnly return what is in the array
+extension Array {
+    var oneAndOnly: Element? {
+        if count == 1 { // faceUpCardIndices.count -> self.count
+            return first // faceUpCardIndices.first -> self.first
+        } else {
+            return nil
+        }
     }
 }
+
+
+
